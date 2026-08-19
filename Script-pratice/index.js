7346246b98ab52e2c6b38b27dfc7765a16ed5863
Ravikrp208 <ravikrp208@gmail.com>
2026-08-19 23:23:01 +0530

    java script Pratice

---
+++ Script-pratice/index.js
@@ -0,0 +1,17 @@
+let name = "Ravi kumar Pandit";
+
+console.log("My name is " + name);
+
+let a = 10;
+let b = 20;
+
+console.log("sum ",a+b);
+
+
+let a = 20;
+let b = 10;
+
+console.log("Addition =", a + b);
+console.log("Subtraction =", a - b);
+console.log("Multiplication =", a * b);
+console.log("Division =", a / b);
