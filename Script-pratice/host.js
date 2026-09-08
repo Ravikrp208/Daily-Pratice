d9860f0752f616c04fb42265fdf50302357a9fd5
Ravikrp208 <ravikrp208@gmail.com>
2026-09-08 19:12:06 +0530

    hosting

---
+++ Script-pratice/host.js
@@ -0,0 +1,16 @@
+// console.log(userName);
+
+// var userName = "Ravi";
+
+// console.log(userName);
+
+//output:-  undefined Ravi
+
+// 2.
+var userName;
+
+console.log(userName);
+
+userName = "Ravi";
+
+console.log(userName);
