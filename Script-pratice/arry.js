8fdda397bf107d1ea0f56283d818696c4e629fdd
Ravikrp208 <ravikrp208@gmail.com>
2026-09-15 22:46:34 +0530

    object class in javascript

--- Script-pratice/arry.js
+++ Script-pratice/arry.js
@@ -4,3 +4,14 @@ fruits.push("Watermelon"); // Add at end
 fruits.shift(); // Remove from beginning
 
 console.log(fruits);
+
+/* object */
+let person = {
+  name: "Ravi",
+  age: 25,
+  city: "Patna",
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
